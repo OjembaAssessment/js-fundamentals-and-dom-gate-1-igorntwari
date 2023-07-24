3333961f3c13e087f1790aed6fae9765ec0e3999
igorntwari <igorntwari28@gmail.com>
2023-07-24 11:08:08 +0300

    exercises link

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,1 +1,22 @@
+function longest (search, str) {
+ 
+    search = document.getElementById("myParagraph").innerHTML;
+    let str = words.split(/\b/);
+    for (let i = 0; i < str.length; i++) {
+        let count = 0;
+     for (let j = 0; j < str.length; j++) {
+         if (str[j] == str[i]) {
+             count++;
+         }
+     }
+     if(count>=freq){
+        most = str[i];
+        freq = count;
+    }
+
+}
+}
+
+
+
 console.log("Exercise 1 - Word Highlighter");
