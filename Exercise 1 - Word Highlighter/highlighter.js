10b74276e3673af1a73e4db8f06b8c4c629d8d9e
igorntwari <igorntwari28@gmail.com>
2023-07-24 13:44:39 +0300

    solved exercise 01

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,22 +1,23 @@
-function longest (search, str) {
- 
-    search = document.getElementById("myParagraph").innerHTML;
-    let str = words.split(/\b/);
-    for (let i = 0; i < str.length; i++) {
-        let count = 0;
-     for (let j = 0; j < str.length; j++) {
-         if (str[j] == str[i]) {
-             count++;
-         }
-     }
-     if(count>=freq){
-        most = str[i];
-        freq = count;
+let  counterwords = ()=>{ 
+ let paragraph = document.getElementById("myParagraph").innerHTML;
+  const words = paragraph.split(" ");
+  const repeatedWords = {};
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    if (repeatedWords[word]) {
+      repeatedWords[word]++;
+    } else {
+      repeatedWords[word] = 1;
     }
+  }
+  const sortedWords = Object.keys(repeatedWords).sort((a, b) => repeatedWords[b] - repeatedWords[a]);
+  for (let i = 0; i < sortedWords.length && i < 5; i++) {
+    const word = sortedWords[i];
+    const myownRegex = new RegExp(`\\b${word}\\b`, "gi");
+    paragraph = paragraph.replace(myownRegex, `<span class="highlight">${word}</span>`);
+  }
 
+  document.getElementById("myParagraph").innerHTML = paragraph;
 }
-}
-
-
-
+counterwords();
 console.log("Exercise 1 - Word Highlighter");
--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,1 +1,40 @@
+let counterElement = document.getElementById('mytime');
+let count = 0;
+function start() {
+
+  intervalId = setInterval(() => {
+    count++;
+    counterElement.textContent = count;
+  }, 1000);
+}
+
+function ADDsecond() {
+  count ++;
+  counterElement.textContent = count;
+}
+
+  function RemoveSeconds() {
+    count--;
+    counterElement.textContent = count;
+  }
+
+  function Reset() {
+    count =0;
+    counterElement.textContent = count;
+  }
+  function stop() {
+    clearInterval(intervalId);
+  }
+
+  
+
+
+
+
+
+
+
+
+
+
 console.log("Exercise 3 - Timer");
